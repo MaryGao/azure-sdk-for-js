52dbded8c54dc43ea51c6e05a9c67dfbc348f155
Mary Gao <yanmeigao1210@gmail.com>
2024-11-13 17:42:47 +0800

    Update the inital request

--- sdk/keyvault/keyvault-secrets/src/lro/keyVaultSecretPoller.ts
+++ sdk/keyvault/keyvault-secrets/src/lro/keyVaultSecretPoller.ts
@@ -1,104 +1,193 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { OperationState } from "@azure/core-lro";
+import { OperationOptions } from "@azure-rest/core-client";
+import { AbortSignalLike } from "@azure/abort-controller";
+import {
+  CancelOnProgress,
+  RunningOperation,
+  OperationResponse,
+  OperationState,
+  createHttpPoller,
+  RawRequest,
+  PollerLike as CorePollerLike,
+} from "@azure/core-lro";
+import { KeyVaultClient } from "../generated/keyVaultClient.js";
+import { initOperation } from "../../../../core/core-lro/dist/esm/poller/operation.js";
 
-// import { OperationOptions } from "@azure-rest/core-client";
-// import { PollOperation, PollOperationState, Poller } from "@azure/core-lro";
-// import { KeyVaultClient } from "../generated/keyVaultClient.js";
-// import { delay } from "@azure/core-util";
-
-// /**
-//  * Common parameters to a Key Vault Secret Poller.
-//  */
-// export interface KeyVaultSecretPollerOptions {
-//   vaultUrl: string;
-//   client: KeyVaultClient;
-//   name: string;
-//   operationOptions?: OperationOptions;
-//   intervalInMs?: number;
-//   resumeFrom?: string;
-// }
 
 /**
- * An interface representing the state of a Key Vault Secret Poller's operation.
+ * PollOperationState contains an opinionated list of the smallest set of properties needed
+ * to define any long running operation poller.
+ *
+ * While the Poller class works as the local control mechanism to start triggering, wait for,
+ * and potentially cancel a long running operation, the PollOperationState documents the status
+ * of the remote long running operation.
+ *
+ * It should be updated at least when the operation starts, when it's finished, and when it's cancelled.
+ * Though, implementations can have any other number of properties that can be updated by other reasons.
  */
-export interface KeyVaultSecretPollOperationState<TResult> extends OperationState<TResult> {
+export interface PollOperationState<TResult> {
   /**
-   * The name of the secret.
+   * True if the operation has started.
    */
-  name: string;
+  isStarted?: boolean;
+  /**
+   * True if the operation has been completed.
+   */
+  isCompleted?: boolean;
+  /**
+   * True if the operation has been cancelled.
+   */
+  isCancelled?: boolean;
+  /**
+   * Will exist if the operation encountered any error.
+   */
+  error?: Error;
+  /**
+   * Will exist if the operation concluded in a result of an expected type.
+   */
+  result?: TResult;
 }
 
-// /**
-//  * Common properties and methods of the Key Vault Secret Pollers.
-//  */
-// export abstract class KeyVaultSecretPoller<
-//   TState extends KeyVaultSecretPollOperationState<TResult>,
-//   TResult,
-// > extends Poller<TState, TResult> {
-//   /**
-//    * Defines how much time the poller is going to wait before making a new request to the service.
-//    */
-//   public intervalInMs: number = 2000;
+/**
+ * Abstract representation of a poller, intended to expose just the minimal API that the user needs to work with.
+ */
+export interface PollerLike<TState extends PollOperationState<TResult>, TResult> {
+  /**
+   * Returns a promise that will resolve once a single polling request finishes.
+   * It does this by calling the update method of the Poller's operation.
+   */
+  poll(options?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<void>;
+  /**
+   * Returns a promise that will resolve once the underlying operation is completed.
+   */
+  pollUntilDone(pollOptions?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<TResult>;
+  /**
+   * Invokes the provided callback after each polling is completed,
+   * sending the current state of the poller's operation.
+   *
+   * It returns a method that can be used to stop receiving updates on the given callback function.
+   */
+  onProgress(callback: (state: TState) => void): CancelOnProgress;
+  /**
+   * Returns true if the poller has finished polling.
+   */
+  isDone(): boolean;
+  /**
+   * Stops the poller. After this, no manual or automated requests can be sent.
+   */
+  stopPolling(): void;
+  /**
+   * Returns true if the poller is stopped.
+   */
+  isStopped(): boolean;
+  /**
+   * Attempts to cancel the underlying operation.
+   * @deprecated `cancelOperation` has been deprecated because it was not implemented.
+   */
+  cancelOperation(options?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<void>;
+  /**
+   * Returns the state of the operation.
+   * The TState defined in PollerLike can be a subset of the TState defined in
+   * the Poller implementation.
+   */
+  getOperationState(): TState;
+  /**
+   * Returns the result value of the operation,
+   * regardless of the state of the poller.
+   * It can return undefined or an incomplete form of the final TResult value
+   * depending on the implementation.
+   */
+  getResult(): TResult | undefined;
+  /**
+   * Returns a serialized version of the poller's operation
+   * by invoking the operation's toString method.
+   */
+  toString(): string;
+}
 
-//   /**
-//    * The method used by the poller to wait before attempting to update its operation.
-//    */
-//   async delay(): Promise<void> {
-//     return delay(this.intervalInMs);
-//   }
-// }
+/**
+ * Common parameters to a Key Vault Key Poller.
+ */
+export interface KeyVaultKeyPollerOptions {
+  vaultUrl: string;
+  client: KeyVaultClient;
+  name: string;
+  operationOptions?: OperationOptions;
+  intervalInMs?: number;
+  resumeFrom?: string;
+}
 
-// /**
-//  * Optional parameters to the KeyVaultSecretPollOperation
-//  */
-// export interface KeyVaultSecretPollOperationOptions {
-//   cancelMessage?: string;
-// }
+export abstract class KeyVaultSecretPoller<TState extends PollOperationState<TResult>, TResult> implements PollerLike<TState, TResult>, RunningOperation<TResult> {
+  protected httpPoller: CorePollerLike<OperationState<TResult>, TResult>;
+  protected options: KeyVaultKeyPollerOptions;
+  constructor(options: KeyVaultKeyPollerOptions) {
+    this.options = options;
+    this.httpPoller = createHttpPoller({
+      sendInitialRequest: this.sendInitialRequest.bind(this),
+      sendPollRequest: this.sendPollRequest.bind(this),
+    }, options);
+  }
+  sendInitialRequest(): Promise<OperationResponse<unknown>> {
+    throw new Error("Method not implemented.");
+  }
+  sendPollRequest(_path: string, _options?: { abortSignal?: AbortSignalLike; }): Promise<OperationResponse<TResult, RawRequest>> {
+    throw new Error("Method not implemented.");
+  }
+  async poll(options?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<void> {
+    await this.httpPoller.poll(options);
+  }
+  pollUntilDone(pollOptions?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<any> {
+    return this.httpPoller.pollUntilDone(pollOptions);
+  }
+  onProgress(callback: (state: any) => void): CancelOnProgress {
+    return this.httpPoller.onProgress(callback);
+  }
+  isDone(): boolean {
+    return this.httpPoller.isDone;
+  }
+  stopPolling(): void {
+    throw new Error("Method not implemented.");
+  }
+  isStopped(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  cancelOperation(_options?: {
+    abortSignal?: AbortSignalLike;
+  }): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  getOperationState(): any {
+    if (!this.httpPoller.operationState) {
+      throw new Error(
+        "Operation state is not available. The poller may not have been started and you could await submitted() before calling getOperationState().",
+      );
+    }
+    return this.httpPoller.operationState;
+  }
+  getResult(): any {
+    return this.httpPoller.result;
+  }
+  toString(): string {
+    if (!this.httpPoller.operationState) {
+      throw new Error(
+        "Operation state is not available. The poller may not have been started and you could await submitted() before calling getOperationState().",
+      );
+    }
+    return JSON.stringify({
+      state: this.httpPoller.operationState,
+    });
+  }
 
-// /**
-//  * Common properties and methods of the Key Vault Secret Poller operations.
-//  */
-// // eslint-disable-next-next no-use-before-define
-// export class KeyVaultSecretPollOperation<
-//   TState extends KeyVaultSecretPollOperationState<TResult>,
-//   TResult,
-// > implements PollOperation<TState, TResult>
-// {
-//   private cancelMessage: string = "";
-
-//   constructor(
-//     public state: TState,
-//     options: KeyVaultSecretPollOperationOptions = {},
-//   ) {
-//     if (options.cancelMessage) {
-//       this.cancelMessage = options.cancelMessage;
-//     }
-//   }
-
-//   /**
-//    * Meant to reach to the service and update the Poller operation.
-//    * @param options - The optional parameters, which is only an abortSignal from \@azure/abort-controller
-//    */
-//   public async update(): Promise<PollOperation<TState, TResult>> {
-//     throw new Error("Operation not supported.");
-//   }
-
-//   /**
-//    * Meant to reach to the service and cancel the Poller operation.
-//    * @param options - The optional parameters, which is only an abortSignal from \@azure/abort-controller
-//    */
-//   public async cancel(): Promise<PollOperation<TState, TResult>> {
-//     throw new Error(this.cancelMessage);
-//   }
-
-//   /**
-//    * Serializes the Poller operation.
-//    */
-//   public toString(): string {
-//     return JSON.stringify({
-//       state: this.state,
-//     });
-//   }
-// }
+}
